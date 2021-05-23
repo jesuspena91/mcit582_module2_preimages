17ff01edf9794399d981c2a01594646faaa369dd
jesuspena91 <jesusp@seas.upenn.edu>
2021-05-22 19:11:04 -0500

    Update hash_preimage.py

--- hash_preimage.py
+++ hash_preimage.py
@@ -1,10 +1,38 @@
+# Use a brute-force algorithm to find a partial preimage.
+# Using the template “hash_preimage.py” write a function called “hash_preimage” that takes a single input, target_string, 
+# where target_string is a string of bits. The function “hash_preimage” should return a single variable x such that the 
+# trailing bits of SHA256(x) matches the target string (not the hash of the target string).
+# Your algorithm should be randomized, i.e., hash_preimage(target_string) should not always return the same partial preimage.
+
 import hashlib
 import os
+import math
 
 def hash_preimage(target_string):
     if not all( [x in '01' for x in target_string ] ):
         print( "Input should be a string of bits" )
         return
-    nonce = b'\x00'
+    
+    random_base = 64
 
-    return( nonce )
+    while True:
+    	random_num = os.urandom(random_base)
+    	hash_random_num = hashlib.sha256(random_num).hexdigest()
+    	int_random_num = int(hash_random_num, base=16)
+    	binary_random_num = bin(int_random_num)[2:]
+
+    	if (binary_random_num[-len(target_string):] == target_string):
+    		break
+
+ 	return(binary_random_num)
+    # nonce = b'\x00'
+    # return(nonce)
+
+    #print(target_string)
+    #print()
+    #print(binary_random_num)
+    #print()
+    #print(binary_random_num[-len(target_string):])
+    #print(type(binary_random_num))
+
+#hash_preimage('011010')
